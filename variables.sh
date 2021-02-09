e749ea169a56d39ec5fd4db91a3bc1cf291c75a3
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-02-10 00:52:06 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 # Repo URLs
-rom_manifest_url="https://index.violetmirrorbot.workers.dev/ROGChinaBeta21.2.3_V1.zip"
+android_manifest_url="https://index.violetmirrorbot.workers.dev/ROGChinaBeta21.2.3_V1.zip"
 
 # Telegram Bot
 your_telegram_id="1428369788"
